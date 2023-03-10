2e42c298f72a1e2dba452e462ac98fae0fea92f0
Yuyi Hao <roshanisingh2354@gmail.com>
2023-03-11 04:41:24 +0530

    TCP

---
+++ ch3/ch3.h
@@ -0,0 +1,29 @@
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<errno.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#endif
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
